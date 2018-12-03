4447ff929fb25086c57a6c877239d3ed746be50d
Logan Dam <logan.dam@entelect.co.za>
2018-12-03 10:57:00 +0200

    Added solutions for Day 1

---
+++ Day1/Part1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day1.Part1
+{
+  using System;
+  using System.IO;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var lines = File.ReadAllLines("..\\input.txt");
+      int n = 0;
+      foreach (var line in lines)
+      {
+        n += int.Parse(line);
+      }
+      Console.WriteLine(n);
+    }
+  }
+}
---
+++ Day1/Part2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Day1.Part2
+{
+  using System;
+  using System.Collections.Generic;
+  using System.IO;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var lines = File.ReadAllLines("..\\input.txt");
+      HashSet<int> frequencies = new HashSet<int>();
+      bool found = false;
+      int current = 0;
+      while (!found)
+      {
+        foreach (var line in lines)
+        {
+          current += int.Parse(line);
+          if (frequencies.TryGetValue(current, out int actual))
+          {
+            found = true;
+            Console.WriteLine(current);
+            break;
+          }
+          else
+          {
+            frequencies.Add(current);
+          }
+        }
+      }
+    }
+  }
+}
